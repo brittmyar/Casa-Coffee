980083b0d8c6ab5227e7737cc783ee5468572de2
Brittany Yarbrough <brittanymyar@gmail.com>
2022-09-12 15:37:45 -0400

    full CRUD

--- models/coffee.js
+++ models/coffee.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const coffeeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    ingredients: { type: String, required: true },
-    instructions: { type: String, required : true}
+    ingredients: { type: String, required: false },
+    instructions: { type: String, required : false}
 });
 
 const Coffee = mongoose.model('Coffee', coffeeSchema);
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Coffee = require('./models/coffee.js');
+const methodOverride = require("method-override")
+
 
 // Database Connnection 
 mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 // Database Connection Error/Success
@@ -18,45 +21,77 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 
 
 // Middleware / gives access to req.body
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"))
 
-
+/////////////
 // Routes
+////////////
+
+// Index
+app.get('/coffee', (req, res) => {
+    Coffee.find({}, (error, allCoffee) => {
+      res.render('index.ejs', {
+        coffee: allCoffee,
+      });  
+    });  
+});
+
+// New
+app.get('/coffee/new', (req, res) => {
+    Coffee.find({}, (error, allCoffee) => {
+        res.render('index.ejs', {
+            coffee: allCoffee,
+        });
+    });
+});
+
+// Delete
+app.delete("/coffee/:id", (req, res) => {
+    Book.findByIdAndRemove(req.params.id, (err, data) => {
+      res.redirect("/coffee")
+    })
+  })
+
+// Update
+app.put("/coffee/:id", (req, res) => {
+    Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      },
+      (error, updatedCoffee) => {
+        res.redirect(`/coffee/${req.params.id}`)
+      }
+    )
+  })
+
 
 // Create
-app.post('/casa-coffee', (req, res) => {
-    res.send('received');
+app.post('/coffee', (req, res) => {
+    Coffee.create(req.body, (error, createdCoffee) => {
+        res.send(createdCoffee);
+    });
 });
 
-app.post('/casa-coffee', (req, res) => {
-    res.send(req.body);
+// Edit
+app.get("/coffee/:id/edit", (req, res) => {
+    Coffee.findById(req.params.id, (error, foundCoffee) => {
+      res.render("edit.ejs", {
+        coffee: foundCoffee,
+      });
+    });
+  });
+
+// Show
+app.get('/coffee/:id', (req, res) => {
+	Coffee.findById(req.params.id, (err, foundCoffee) => {
+		res.render('show.ejs', {
+            coffee: foundCoffee,
+        });
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Listener 
